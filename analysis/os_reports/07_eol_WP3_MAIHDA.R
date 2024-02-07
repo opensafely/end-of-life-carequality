164d1d794174fe6540f01e3af4876a9dea654d87
Miranda Davies <137781565+NTMDavies@users.noreply.github.com>
2024-02-07 11:33:16 +0000

    Amending output label

--- analysis/os_reports/07_eol_WP3_MAIHDA.R
+++ analysis/os_reports/07_eol_WP3_MAIHDA.R
@@ -45,18 +45,18 @@ df <- read_csv(file = here::here("output", "os_reports", "input_os_reports.csv.g
 
 # Counts of places of death by ethnicity for all deaths + cancer only - not for release
 
-deaths_ethnicity_place_all <- df %>%
+deaths_ethnicity_place_all_raw <- df %>%
   group_by(ethnicity_Combined, pod_ons_new) %>%
   summarise(count = n()) %>%
 
-fwrite(deaths_ethnicity_place_all, here::here("output", "os_reports", "eol_service", "deaths_ethnicity_place_all_raw.csv"))
+fwrite(deaths_ethnicity_place_all_raw, here::here("output", "os_reports", "eol_service", "deaths_ethnicity_place_all_raw.csv"))
 
-deaths_ethnicity_place_cancer <- df %>%
+deaths_ethnicity_place_cancer_raw <- df %>%
   filter(codgrp == "Cancer") %>%
   group_by(ethnicity_Combined, pod_ons_new) %>%
   summarise(count = n()) %>%
 
-fwrite(deaths_ethnicity_place_cancer, here::here("output", "os_reports", "eol_service", "deaths_ethnicity_place_cancer_raw.csv"))
+fwrite(deaths_ethnicity_place_cancer_raw, here::here("output", "os_reports", "eol_service", "deaths_ethnicity_place_cancer_raw.csv"))
 
 # Descriptive analysis to inform modelling - counts of age_band / sex / ethnicity and imd_rounded 
 
