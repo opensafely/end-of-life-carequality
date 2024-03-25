a752b0b60b4569cc53f3c295601c257ba6d5992a
Miranda Davies <137781565+NTMDavies@users.noreply.github.com>
2024-03-25 10:11:38 +0000

    File set-up for ethnicity analysis

---
+++ analysis/os_reports/08_eol_Ethnicity_breakdown.R
@@ -0,0 +1,44 @@
+###############################################################################
+# CSV files for ethnicity analysis
+# Date: 25.03.24
+# Author: Miranda & Sophie 
+# Aim: Descriptive analysis to explore breakdown of patients at end of life by ethnicity in combination with additional characteristics
+# Note: Files are created with and without rounding / redaction. Non-rounded/redacted files are not for release
+###############################################################################
+
+# Load packages
+
+library(tidyverse)
+library(lubridate)
+library(dplyr)
+library(data.table)
+
+# Create folder structure
+
+fs::dir_create("output", "os_reports", "eol_service")
+
+# Code settings
+
+startdate <- dmy("01-01-2022")
+enddate <- dmy("31-12-2023")
+
+# Prepare data
+
+df <- read_csv(file = here::here("output", "os_reports", "input_os_reports.csv.gz")) %>%
+  mutate(dod_ons = as_date(dod_ons)
+         , Ethnicity_2 = case_when(ethnicity_Combined == "White" ~ "White"
+                             , ethnicity_Combined == "Asian or Asian British" | ethnicity_Combined == "Black or Black British" | ethnicity_Combined == "Mixed" | ethnicity_Combined == "Chinese or Other Ethnic Groups" | ethnicity_Combined == "Not stated" ~ "All other ethnic groups")
+         , study_month = floor_date(dod_ons, unit = "month")
+         , pod_ons_new = case_when(pod_ons == "Elsewhere" 
+                                   | pod_ons == "Other communal establishment" ~ "Elsewhere/other"
+                                   , TRUE ~ as.character(pod_ons))
+         , cod_ons_3 = str_sub(cod_ons, 1, 3)
+         , cod_ons_4 = str_sub(cod_ons, 1, 5)
+         , codgrp = case_when(cod_ons_4 %in% c("U071", "U072") ~ "Covid-19"
+                             , cod_ons_3 >= "J09" & cod_ons_3 <= "J18" ~ "Flu and pneumonia"
+                             , (cod_ons_3 >= "J00" & cod_ons_3 <= "J08") | (cod_ons_3 >= "J19" & cod_ons_3 <= "J99")  ~ "Other respiratory diseases"
+                             , cod_ons_3 %in% c("F01", "F03", "G30") ~ "Dementia and Alzheimer's disease"
+                             , cod_ons_3 >= "I00" & cod_ons_3 <= "I99" ~ "Circulatory diseases"
+                             , cod_ons_3 >= "C00" & cod_ons_3 <= "C99" ~ "Cancer"
+                             , TRUE ~ "All other causes")) %>%
+  filter(study_month >= startdate & study_month <= enddate
