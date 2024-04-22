c3478bd788f151a9c9e23ca469380886c3333c7c
Miranda Davies <137781565+NTMDavies@users.noreply.github.com>
2024-04-22 14:53:05 +0100

    Output code amendments
    Addition of 'row.names = FALSE' to clean up rows.

--- analysis/os_reports/07_eol_WP3_MAIHDA.R
+++ analysis/os_reports/07_eol_WP3_MAIHDA.R
@@ -141,7 +141,7 @@ str(summary(fm1))
 beta0 <- summary(fm1)$coefficients$cond[1,1]
 beta0
 
-write.csv (beta0, file = 'beta0.csv')
+write.csv (beta0, file = 'beta0.csv', row.names = FALSE)
 beta0 <- read_csv(file =  "beta0.csv")
 fwrite(beta0, here::here("output", "os_reports", "WP3", "beta0.csv"))
 
@@ -151,40 +151,40 @@ str(summary(fm1))
 sigma2u <- summary(fm1)$varcor$cond$strata[1,1]
 sigma2u
 
-write.csv(sigma2u, file = 'sigma2u.csv')
-sigma2u.csv <-read_csv(file = "sigma2.csv")
+write.csv(sigma2u, file = 'sigma2u.csv', row.names = FALSE)
+sigma2u <-read_csv(file = "sigma2u.csv")
 fwrite(sigma2u, here::here("output", "os_reports", "WP3", "sigma2u.csv"))
 
 # Marginal expectation (approximately = mean no. of A&E attendances)
 expectation <- exp(beta0 + sigma2u/2)
 expectation
 
-write.csv(expectation, file = 'expectation.csv')
-expectation.csv <-read_csv(file = "expectation.csv")
+write.csv(expectation, file = 'expectation.csv', row.names = FALSE)
+expectation <-read_csv(file = "expectation.csv")
 fwrite(expectation, here::here("output", "os_reports", "WP3", "expectation.csv"))
 
 # Marginal variance
 variance <- expectation + expectation^2*(exp(sigma2u) - 1)
 variance
 
-write.csv(variance, file = 'variance.csv')
-variance.csv <-read_csv(file = "variance.csv")
+write.csv(variance, file = 'variance.csv', row.names = FALSE)
+variance <-read_csv(file = "variance.csv")
 fwrite(variance, here::here("output", "os_reports", "WP3", "variance.csv"))
 
 # Marginal variance: Level-2 component (Variance between clusters - based on age groups, sex, etc)
 variance2 <- expectation^2*(exp(sigma2u) - 1)
 variance2
 
-write.csv(variance2, file = 'variance2.csv')
-variance2.csv <-read_csv(file = "variance2.csv")
+write.csv(variance2, file = 'variance2.csv', row.names = FALSE)
+variance2 <-read_csv(file = "variance2.csv")
 fwrite(variance2, here::here("output", "os_reports", "WP3", "variance2.csv"))
 
 # Marginal variance: Level-1 component (variance within clusters)
 variance1 <- expectation
 variance1
 
-write.csv(variance1, file = 'variance1.csv')
-variance1.csv <-read_csv(file = "variance1.csv")
+write.csv(variance1, file = 'variance1.csv', row.names = FALSE)
+variance1 <-read_csv(file = "variance1.csv")
 fwrite(variance1, here::here("output", "os_reports", "WP3", "variance1.csv"))
 
 # Level-2 VPC (variance partition coefficient)
@@ -195,16 +195,16 @@ fwrite(variance1, here::here("output", "os_reports", "WP3", "variance1.csv"))
 vpc2 <- variance2/(variance2 + variance1)
 vpc2
 
-write.csv(vpc2, file = 'vpc2.csv')
-vpc2.csv <-read_csv(file = "vpc2.csv")
+write.csv(vpc2, 'vpc2.csv', row.names = FALSE)
+vpc2 <-read_csv(file = "vpc2.csv")
 fwrite(vpc2, here::here("output", "os_reports", "WP3", "vpc2.csv"))
 
 # Level-1 VPC (variance partition coefficient - variance at the individual level)
 vpc1 <- variance1/(variance2 + variance1)
 vpc1
 
-write.csv(vpc1, file = 'vpc1.csv')
-vpc1.csv <-read_csv(file = "vpc1.csv")
+write.csv(vpc1, 'vpc1.csv', row.names = FALSE)
+vpc1 <-read_csv(file = "vpc1.csv")
 fwrite(vpc1, here::here("output", "os_reports", "WP3", "vpc1.csv"))
 
 ##################################################################################################################################
@@ -348,3 +348,4 @@ vpc2
 # Level-1 VPC
 vpc1 <- variance1/(variance5 + variance4 + variance3 + variance2 + variance1)
 vpc1
+
