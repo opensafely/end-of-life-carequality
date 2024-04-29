6ed4351d1954b523d214de3f32afac3548aa2df0
Miranda Davies <137781565+NTMDavies@users.noreply.github.com>
2024-04-29 13:56:43 +0100

    Adding GP interactions as MAIHDA outcome variable

--- analysis/os_reports/07_eol_WP3_MAIHDA.R
+++ analysis/os_reports/07_eol_WP3_MAIHDA.R
@@ -134,6 +134,96 @@ GLM_age_band <- df %>%
 fwrite(GLM_age_band, here::here("output", "os_reports", "WP3", "GLM_age_band.csv"))
 
 #########################################################################################################################################
+# Outcome variable = GP interactions
+# Form intersectional strata (80 strata in total)
+
+GP_MAIHDA <-df %>%
+  group_by(sex, age_band, Ethnicity_2, imd_quintile) %>% 
+  dplyr::mutate(strata = cur_group_id(), na.rm = TRUE)
+
+# Model 1  - includes a strata random intercept to account for clustering by strata #
+# Calculate simple intersectional model (Null model - Captures variation without considering the impact of any specific predictor)
+# Poisson model = model for count data
+
+GPfm1 <- glmmTMB(gp_1m ~ 1 + (1|strata), data = GP_MAIHDA, family = poisson)
+summary(GPfm1)
+
+# Intercept (mean no. of A&E attendances when all predictors = 0)
+str(summary(GPfm1))
+GPbeta0 <- summary(GPfm1)$coefficients$cond[1,1]
+GPbeta0
+
+write.csv (GPbeta0, file = 'GPbeta0.csv', row.names = FALSE)
+GPbeta0 <- read_csv(file =  "GPbeta0.csv")
+fwrite(GPbeta0, here::here("output", "os_reports", "WP3", "GPbeta0.csv"))
+
+# Cluster variance
+str(summary(GPfm1))
+GPsigma2u <- summary(GPfm1)$varcor$cond$strata[1,1]
+GPsigma2u
+
+write.csv(GPsigma2u, file = 'GPsigma2u.csv', row.names = FALSE)
+GPsigma2u <-read_csv(file = "GPsigma2u.csv")
+fwrite(GPsigma2u, here::here("output", "os_reports", "WP3", "GPsigma2u.csv"))
+
+# Marginal expectation (approximately = mean no. of A&E attendances)
+GPexpectation <- exp(GPbeta0 + GPsigma2u/2)
+GPexpectation
+
+write.csv(GPexpectation, file = 'GPexpectation.csv', row.names = FALSE)
+GPexpectation <-read_csv(file = "GPexpectation.csv")
+fwrite(GPexpectation, here::here("output", "os_reports", "WP3", "GPexpectation.csv"))
+
+# Marginal variance
+GPvariance <- GPexpectation + GPexpectation^2*(exp(GPsigma2u) - 1)
+GPvariance
+
+write.csv(GPvariance, file = 'GPvariance.csv', row.names = FALSE)
+GPvariance <-read_csv(file = "GPvariance.csv")
+fwrite(GPvariance, here::here("output", "os_reports", "WP3", "GPvariance.csv"))
+
+# Marginal variance: Level-2 component (Variance between clusters - based on age groups, sex, etc)
+GPvariance2 <- GPexpectation^2*(exp(GPsigma2u) - 1)
+GPvariance2
+
+write.csv(GPvariance2, file = 'GPvariance2.csv', row.names = FALSE)
+GPvariance2 <-read_csv(file = "GPvariance2.csv")
+fwrite(GPvariance2, here::here("output", "os_reports", "WP3", "GPvariance2.csv"))
+
+# Marginal variance: Level-1 component (variance within clusters)
+GPvariance1 <- GPexpectation
+GPvariance1
+
+write.csv(GPvariance1, file = 'GPvariance1.csv', row.names = FALSE)
+GPvariance1 <-read_csv(file = "GPvariance1.csv")
+fwrite(GPvariance1, here::here("output", "os_reports", "WP3", "GPvariance1.csv"))
+
+# Level-2 VPC (variance partition coefficient)
+# VPC = proportion of the total variance located at the strata level - the global measure of intersectionality. 
+# VPC = same as ICC
+# A high VPC means the strata are useful for understanding differences in A&E attendances. 
+# Where VPC = 0, strata = random sample from the population not relevant to understanding A&E attendances at end of life. 
+GPvpc2 <- GPvariance2/(GPvariance2 + GPvariance1)
+GPvpc2
+
+write.csv(GPvpc2, 'GPvpc2.csv', row.names = FALSE)
+GPvpc2 <-read_csv(file = "GPvpc2.csv")
+fwrite(GPvpc2, here::here("output", "os_reports", "WP3", "GPvpc2.csv"))
+
+# Level-1 VPC (variance partition coefficient - variance at the individual level)
+GPvpc1 <- GPvariance1/(GPvariance2 + GPvariance1)
+GPvpc1
+
+write.csv(GPvpc1, 'GPvpc1.csv', row.names = FALSE)
+GPvpc1 <-read_csv(file = "GPvpc1.csv")
+fwrite(GPvpc1, here::here("output", "os_reports", "WP3", "GPvpc1.csv"))
+
+
+
+
+#########################################################################################################################################
+
+# Outcome variable = A&E attendances
 
 # Form intersectional strata (80 strata in total)
 
